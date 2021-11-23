9a9c6e2fc0ac77acf9ff7feda9a6f208a5bafcae
Grumpylama <65866382+Grumpylama@users.noreply.github.com>
2021-11-23 23:54:11 +0100

    Added Lines

---
+++ Function.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+namespace ThiccShapes
+{
+    class Line
+    {
+        
+        Point P1;
+        Point P2;
+        
+        public Line(Point p1, Point p2)
+        {
+            P1 = p1;
+            P2 = p2;
+        }
+    }
+}
--- Triangle.cs
+++ Triangle.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace ThiccShapes
 {
@@ -7,12 +8,19 @@ namespace ThiccShapes
         Point P1;
         Point P2;
         Point P3;
-    
+
+        
         public static int ShapeScore = 1;
+        List<Line> lines = new List<Line>();
+
+        static void makeFunctions()
+        {
+
+        }
         //Contancted to make standalone triangles from x, y and perimiter
         public Triangle(int x, int y, int Perimeter)
         {
-            
+
         }
         //Contancted to make triangles that contruct other shapes
         //Send null or anything to overloader
@@ -20,8 +28,13 @@ namespace ThiccShapes
         {
             P1 = p1;
             P2 = p2;
-            P3 = p3;            
-        }        
+            P3 = p3;
+            lines.Add(new Line(p1, p2));
+            lines.Add(new Line(p2, p3));
+            lines.Add(new Line(p3, p1));
+
+        }
+
         public bool Inside(Point p)
         {
             return true;

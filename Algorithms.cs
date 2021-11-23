aeb1353c535e7568fe2bfae7c4b7e8b75e27ca9e
Krenar Manxhuka <manxhuka01@hotmail.com>
2021-11-24 00:31:12 +0100

    IntersectLines

--- Algorithms.cs
+++ Algorithms.cs
@@ -138,9 +138,19 @@ namespace ThiccShapes
             //Denominator
             double D = (x1 - x2)*(y3 - y4) - (y1 - y2)*(x3 - x4);
 
+            if(D == 0)
+            {return false;}
+
             //X and Y values of intersection point
-            double Px = ((x1 * y2 - y1*x2)*(x3 - x4) - (x1 - x2)*(x3*y4 - y3*x4));
-            double Py = ((x1*y2 - y1*x2)*(y3 - y4) - (y1 - y2)*(x3*y4 - y3*x4));
+            double t = ((x1 - x3)*(y3 - y4) - (y1 - y3)*(x3 - x4))/D;
+            double u = ((x1 - x3)*(y1 - y2) - (y1 - y3)*(x3 - x4))/D;
+
+            if(t >= 0 && t <= 1 && u >= 0 && u <= 1)
+            {
+                return true;
+            }
+
+            return false;
         }
     }
 }
